1b62fd1b5f1a1fb2c54cbc87ec272ea3ccd91fd1
Ari Durlacher <arikodcode@gmail.com>
2025-12-07 12:46:38 +0200

    create initGame function and fixed shuffle file and delete log from createDeck file

--- utils/shuffle.js
+++ utils/shuffle.js
@@ -1,26 +1,15 @@
+export default function shuffle(deck) {
+  let count = 0;
 
+  while (count <= 1000) {
+    let index1 = Math.floor(Math.random() * deck.length);
+    let index2 = Math.floor(Math.random() * deck.length);
 
-export default function shuffle(deck){
+    if (index1 !== index2) {
+      [deck[index1], deck[index2]] = [deck[index2], deck[index1]];
 
-    let count = 0;
-
-    while(count <= 1000){
-
-        let index1 = Math.floor(Math.random() * 52);
-        let index2 = Math.floor(Math.random() * 52);
-
-        if (index1 !== index2){
-
-            let temp = deck[index1];
-            deck[index1] = deck[index2];
-            deck[index2] = deck[temp];
-
-            count ++
-
-        };
-
-    return deck;
-
-    };
-
-};
+      count++;
+    }
+  }
+  return deck;
+}

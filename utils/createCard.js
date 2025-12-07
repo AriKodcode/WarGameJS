b9ff681d412940cca69f7ebc118cadbc48f13cef
meir_p <meir12345p@gmail.com>
2025-12-07 16:09:24 +0200

    value add in  createCarte function

--- utils/createCard.js
+++ utils/createCard.js
@@ -1,7 +1,22 @@
 export default function createCard(rank, suite) {
+  let value = 0;
+  const listOfCards = ["2", "3", "4", "5", "6", "7", "8", "9", "10"];
+  if (rank in listOfCards) {
+    value = Number(rank);
+  } else if (rank === "j") {
+    value = 11;
+  } else if (rank === "Q") {
+    value = 12;
+  } else if (rank === "K") {
+    value = 13;
+  } else if (rank === "A") {
+    value = 14;
+  }
+
   let card = {
     rank: rank,
     suite: suite,
+    value: value,
   };
 
   return card;

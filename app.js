20f20abd76b9ad80874bdd5a3a08c7bc686d327f
Ari Durlacher <arikodcode@gmail.com>
2025-12-07 15:53:48 +0200

    create app function and added log in playRound function

---
+++ app.js
@@ -0,0 +1,36 @@
+import playRound from './gameLogic/playRound.js';
+import initGmae from './gameLogic/initGame.js';
+
+function App() {
+  console.log('wellcom to WAR GAME');
+  let arryeAndCards = initGmae();
+  let p1 = arryeAndCards['player_1'];
+  let p2 = arryeAndCards['player_2'];
+  let deck = arryeAndCards['deck'];
+  let game = true;
+  while (game) {
+    if (p1['hand'].length === 0) {
+      break;
+    }
+   playRound(p1,p2) 
+  }
+  if(p1['wonPile'].length> p2['wonPile'].length){
+    console.log(`summery:
+        player 1 have ${p1['wonPile'].length} cards.
+        player 2 have ${p2['wonPile'].length} cards.
+        player 1 win!`);
+  }
+  else if(p2['wonPile'].length > p1['wonPile'].length){
+    console.log(`summery:
+        player 1 have ${p1['wonPile'].length} cards.
+        player 2 have ${p2['wonPile'].length} cards.
+        player 2 win!`);
+  }
+  else(p2['wonPile'].length === p1['wonPile'].length){
+    console.log(`summery:
+        player 1 have ${p1['wonPile'].length} cards.
+        player 2 have ${p2['wonPile'].length} cards.
+        its Draw!`);
+  }
+  
+}
--- gameLogic/playRound.js
+++ gameLogic/playRound.js
@@ -1,18 +1,22 @@
-import compareCards from "../utils/compareCards.js";
+import compareCards from '../utils/compareCards.js';
 export default function playRound(player1, player2) {
-  let cardPlayer1 = player1["hand"].pop(0);
-  let cardPlayer2 = player2["hand"].pop(0);
-
-  if (compareCards(cardPlayer1["value"], cardPlayer2["value"] === "p1")) {
-    player1["wonPile"].push(cardPlayer1, cardPlayer2);
+  console.log(`player 1 card: ${player1['hand'][0]}
+    plaer 2 card: ${player2['hand'][0]}`);
+  let cardPlayer1 = player1['hand'].pop(0);
+  let cardPlayer2 = player2['hand'].pop(0);
+  if (compareCards(cardPlayer1['value'], cardPlayer2['value'] === 'p1')) {
+    player1['wonPile'].push(cardPlayer1, cardPlayer2);
+    console.log('player 1 take the round');
   } else if (
-    compareCards(cardPlayer1["value"], cardPlayer2["value"] === "p2")
+    compareCards(cardPlayer1['value'], cardPlayer2['value'] === 'p2')
   ) {
-    player2["wonPile"].push(cardPlayer1, cardPlayer2);
+    player2['wonPile'].push(cardPlayer1, cardPlayer2);
+    console.log('player 2 take the round');
   } else if (
-    compareCards(cardPlayer1["value"], cardPlayer2["value"] === "WAR")
+    compareCards(cardPlayer1['value'], cardPlayer2['value'] === 'WAR')
   ) {
-    cardPlayer1["wonPile"].push(cardPlayer1);
-    cardPlayer2["wonPile"].push(cardPlayer2);
+    cardPlayer1['wonPile'].push(cardPlayer1);
+    cardPlayer2['wonPile'].push(cardPlayer2);
+    console.log('Draw');
   }
 }
